11865c7aae58c20ed1154dd58d2a0e9780cda40f
manviny <manol@manol.local>
2014-10-22 12:28:41 +0200

    moment lang es-ES

--- app/scripts/services/cal.js
+++ app/scripts/services/cal.js
@@ -10,6 +10,52 @@
 angular.module('mcalendarioAppApp')
   .factory('cal', function ($q, $rootScope, $http, $filter) {
     
+moment.lang('es', {
+  months: 'enero_febrero_marzo_abril_mayo_junio_julio_agosto_septiembre_octubre_noviembre_diciembre'.split('_'),
+  monthsShort: 'ene._feb._mar._abr._may_jun_jul._ago_sept._oct._nov._dic.'.split('_'),
+  weekdays: 'Domingo_Lunes_Martes_Miércoles_Jueves_Viernes_Sabado'.split('_'),
+  weekdaysShort: 'dom._lun._mar._mie._jue._vie._sa.'.split('_'),
+  weekdaysMin: 'Do_Lu_Ma_Mi_Ju_Vi_Sa'.split('_'),
+  longDateFormat: {
+    LT: 'HH:mm',
+    L: 'DD/MM/YYYY',
+    LL: 'D MMMM YYYY',
+    LLL: 'D MMMM YYYY LT',
+    LLLL: 'dddd D MMMM YYYY LT'
+  },
+  calendar: {
+    sameDay: '[Aujourdhui à] LT',
+    nextDay: '[Demain à] LT',
+    nextWeek: 'dddd [à] LT',
+    lastDay: '[Hier à] LT',
+    lastWeek: 'dddd [dernier à] LT',
+    sameElse: 'L'
+  },
+  relativeTime: {
+    future: 'dans %s',
+    past: 'il y a %s',
+    s: 'quelques secondes',
+    m: 'une minute',
+    mm: '%d minutes',
+    h: 'une heure',
+    hh: '%d heures',
+    d: 'un jour',
+    dd: '%d jours',
+    M: 'un mois',
+    MM: '%d mois',
+    y: 'une année',
+    yy: '%d années'
+  },
+  ordinal: function (number) {
+    return number + (number === 1 ? 'er' : 'ème');
+  },
+  week: {
+    dow: 1, // Monday is the first day of the week.
+    doy: 4 // The week that contains Jan 4th is the first week of the year.
+  }
+});
+
+moment.lang('es');
 
     var calendarioFechaSeleccionada;
 
